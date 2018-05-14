7a4c5e52eaea4acb15a54c2e7328ca43bf859382
hpb <hpb21@126.com>
2018-05-14 17:17:51 +0800

    更新rnn引入

--- src/model/seq2seq_model.py
+++ src/model/seq2seq_model.py
@@ -29,6 +29,7 @@ import tensorflow as tf
 #from tensorflow.nn import rnn, rnn_cell
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import variable_scope
+from tensorflow.python.ops import rnn_cell_impl
 
 from .seq2seq import model_with_buckets
 from .seq2seq import embedding_attention_decoder
@@ -83,23 +84,23 @@ class Seq2SeqModel(object):
         self.buckets = buckets
         self.encoder_masks = encoder_masks
 
-        # Create the internal multi-layer cell for our RNN.
-        single_cell = tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(attn_num_hidden, forget_bias=0.0, state_is_tuple=False)
+        # Create the internal multi-layer cell for our RNN
+        single_cell = rnn_cell_impl.BasicLSTMCell(attn_num_hidden, forget_bias=0.0, state_is_tuple=False)
         if use_gru:
             print("using GRU CELL in decoder")
-            single_cell = tf.contrib.rnn.core_rnn_cell.GRUCell(attn_num_hidden)
+            single_cell = rnn_cell_impl.GRUCell(attn_num_hidden)
         cell = single_cell
 
         if attn_num_layers > 1:
-            cell = tf.contrib.rnn.core_rnn_cell.MultiRNNCell([single_cell] * attn_num_layers, state_is_tuple=False)
+            cell = rnn_cell_impl.MultiRNNCell([single_cell] * attn_num_layers, state_is_tuple=False)
 
         # The seq2seq function: we use embedding for the input and attention.
         def seq2seq_f(lstm_inputs, decoder_inputs, seq_length, do_decode):
 
             num_hidden = attn_num_layers * attn_num_hidden
-            lstm_fw_cell = tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(num_hidden, forget_bias=0.0, state_is_tuple=False)
+            lstm_fw_cell = rnn_cell_impl.BasicLSTMCell(num_hidden, forget_bias=0.0, state_is_tuple=False)
             # Backward direction cell
-            lstm_bw_cell = tf.contrib.rnn.core_rnn_cell.BasicLSTMCell(num_hidden, forget_bias=0.0, state_is_tuple=False)
+            lstm_bw_cell = rnn_cell_impl.BasicLSTMCell(num_hidden, forget_bias=0.0, state_is_tuple=False)
 
             pre_encoder_inputs, output_state_fw, output_state_bw = tf.contrib.rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, lstm_inputs,
                 initial_state_fw=None, initial_state_bw=None,
